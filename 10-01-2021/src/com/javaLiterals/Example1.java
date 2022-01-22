5cfb620011d8bea14b8668ca4cdabc22362dafd6
Dinesh1121 <Dinesh1121@Dhanashree>
2022-01-22 15:18:37 +0530

    third commit

---
+++ 10-01-2021/src/com/javaLiterals/Example1.java
@@ -0,0 +1,23 @@
+package com.javaLiterals;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+int productId = 2003;
+String productName = "bajaj hair oil";
+char productgrade ='A';
+String productAddress ="Nashik";
+float productPrize=199;
+String productManufacture="bajaj pvt ltd";
+
+System.out.println(productId);
+System.out.println(productName);
+System.out.println(productgrade);
+System.out.println(productAddress);
+System.out.println(productPrize);
+System.out.println(productManufacture);
+
+
+	}
+
+}
---
+++ 10-01-2021/src/com/javaLiterals/Example2.java
@@ -0,0 +1,24 @@
+package com.javaLiterals;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		String brandName = "OPPO A5S";
+		String colour ="BLACK";
+		float Prize=12000;
+		String processor="Octa-core";
+		String ram="4GB";
+		
+
+		System.out.println("Name:" +brandName);
+		System.out.println("Colour:" +colour);
+		System.out.println("Prize:" +Prize);
+		System.out.println("processor:" +processor);
+		System.out.println("ram:" +ram);
+		
+		
+
+
+	}
+
+}
